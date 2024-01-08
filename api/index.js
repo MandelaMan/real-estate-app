2d2031443f2f5a38267743126af249ab86aefb83
MandelaMan <nelsonomoro@gmail.com>
2024-01-08 14:47:24 +0300

    connection to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch(() => {
+    console.log("Error with connection ");
+  });
 
 app.listen(3000, () => {
   console.log("Server is up and running " + 3000);
