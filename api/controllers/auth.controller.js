fcf8821e9aea8707e597958963adaa78d1ee7875
MandelaMan <nelsonomoro@gmail.com>
2024-01-15 10:02:24 +0300

    added login for user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,43 @@
 const User = require("../models/user.model.js");
 const { genSaltSync, hashSync, compareSync, compare } = require("bcrypt");
 const { errorHandler } = require("../utils/error.js");
+const { sign } = require("jsonwebtoken");
 
 module.exports = {
+  logIn: async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+      const isValidUser = await User.findOne({ email });
+
+      if (!isValidUser) {
+        return next(errorHandler(404, "Invalid credentials"));
+      }
+
+      const validPassword = compareSync(
+        password.toString(),
+        isValidUser.password
+      );
+
+      if (!validPassword) {
+        return next(errorHandler(404, "Invalid credentials"));
+      }
+
+      const token = sign({ id: isValidUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      // To return everything else and exclude password
+      const { password: pass, ...remaining_info } = isValidUser._doc;
+
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(remaining_info);
+    } catch (err) {
+      return next(errorHandler(500, "Internal server error"));
+    }
+  },
   createAccount: async (req, res, next) => {
     const body = req.body;
     const salt = genSaltSync(10);
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createAccount } = require("../controllers/auth.controller.js");
+const { createAccount, logIn } = require("../controllers/auth.controller.js");
 
 const router = express.Router();
 
 router.post("/signup", createAccount);
+router.post("/signin", logIn);
 
 module.exports = router;
